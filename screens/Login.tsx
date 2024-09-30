6f929e78e9d0495d7efd84b9110b4a3e1eb23e5b
r9988.2588 <r9988.2588@gmail.com>
2024-09-30 10:18:30 +0000

    added textinputs

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,18 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
 
-const Login = () => {
+const Signup = () => {
+    const navigation = useNavigation();
+
+    const [name, setName] = useState('');
+    const [isFocusedInput, setIsFocusedInput] = useState('')
+
     return (
         <SafeAreaView style={styles.background}>
+
+            <Text style={styles.text}>To get started, first enter your phone, email or @username </Text>
+
             <View style={styles.container}>
-                <Text style={styles.text}>Login</Text>
+                <TextInput
+                    style={[styles.input, isFocusedInput === 'name' && styles.focusInput]}
+                    placeholder="Phone, email or username"
+                    value={name}
+                    onChangeText={newLogin => setName(newLogin)}
+                    placeholderTextColor="gray"
+                    maxLength={50}
+                    cursorColor={'#3493d6'}
+                    onFocus={() => setIsFocusedInput('name')}
+                    onBlur={() => setIsFocusedInput('')}
+                />
+
+
+            </View>
+
+            <View style={styles.btn}>
+                <Pressable onPress={() => navigation.navigate('Login')} style={styles.forgotPassBtn} android_ripple={{ color: '#00000035', borderless: false, foreground: true }}>
+                    <Text style={styles.forgotPassText}>Forgot password?</Text>
+                </Pressable>
+
+                <Pressable onPress={() => navigation.navigate('Login')} style={styles.nextBtn} android_ripple={{ color: '#fffff', borderless: false, foreground: true }}>
+                    <Text style={styles.nextText}>Next</Text>
+                </Pressable>
             </View>
         </SafeAreaView>
     )
 }
 
-export default Login;
+export default Signup;
 
 const styles = StyleSheet.create({
 
@@ -23,13 +54,68 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        // justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 140,
     },
 
     text: {
-        color: 'white',
-        fontSize: 20,
+        fontSize: 30,
         fontWeight: 'bold',
+        color: 'white',
+        marginLeft: 20
+    },
+
+    input: {
+        fontSize: 18,
+        padding: 18,
+        borderWidth: 1,
+        borderColor: 'white',
+        marginTop: -100,
+        paddingLeft: 20,
+        width: '90%',
+        borderRadius: 6,
+        color: 'white'
+    },
+
+    focusInput: {
+        borderWidth: 2,
+        borderColor: '#3493d6'
+    },
+
+    forgotPassBtn: {
+        borderRadius: 50,
+        width: '42%',
+        padding: 5,
+        borderWidth: 1,
+        borderColor: 'white',
+        margin: 'auto',
+        marginBottom: 10,
+    },
+
+    forgotPassText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+
+    nextBtn: {
+        backgroundColor: 'white',
+        borderRadius: 50,
+        width: '22%',
+        margin: 'auto',
+        padding: 5,
+        marginBottom: 10,
+    },
+
+    nextText: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+
+    btn: {
+        flex: 1,
+        flexDirection: 'row',
     }
 })
--- screens/Signup.tsx
+++ screens/Signup.tsx
@@ -1,12 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
 
 const Signup = () => {
+    const navigation = useNavigation();
+
+    const [name, setName] = useState('');
+    const [isFocusedInput, setIsFocusedInput] = useState('')
+    const [isPhOrEmail, setIsPhOrEmail] = useState('')
+    const [dob, setDob] = useState('');
+
     return (
         <SafeAreaView style={styles.background}>
+
+            <Text style={styles.text}>Create your account</Text>
+
             <View style={styles.container}>
-                <Text style={styles.text}>Signup</Text>
+                <TextInput
+                    style={[styles.input, isFocusedInput === 'name' && styles.focusInput]}
+                    placeholder="Your name"
+                    value={name}
+                    onChangeText={newName => setName(newName)}
+                    placeholderTextColor="gray"
+                    maxLength={50}
+                    cursorColor={'#3493d6'}
+                    onFocus={() => setIsFocusedInput('name')}
+                    onBlur={() => setIsFocusedInput('')}
+                />
+
+                <TextInput
+                    style={[styles.input, isFocusedInput === 'ph' && styles.focusInput]}
+                    placeholder="Phone number or email address"
+                    value={isPhOrEmail}
+                    onChangeText={newPhOrEmail => setIsPhOrEmail(newPhOrEmail)}
+                    placeholderTextColor="gray"
+                    cursorColor={'#3493d6'}
+                    onFocus={() => setIsFocusedInput('ph')}
+                    onBlur={() => setIsFocusedInput('')}
+                />
+
+                <TextInput
+                    style={[styles.input, isFocusedInput === 'dob' && styles.focusInput]}
+                    placeholder="Date of birth in DD/MM/YY"
+                    value={dob}
+                    onChangeText={newDob => setDob(newDob)}
+                    placeholderTextColor="gray"
+                    cursorColor={'#3493d6'}
+                    onFocus={() => setIsFocusedInput('dob')}
+                    onBlur={() => setIsFocusedInput('')}
+
+                />
+            </View>
+
+            <View>
+                <Pressable onPress={() => navigation.navigate('Login')} style={styles.nextBtn} android_ripple={{ color: '#00000035', borderless: false, foreground: true }}>
+                    <Text style={styles.nextText}>Next</Text>
+                </Pressable>
             </View>
         </SafeAreaView>
     )
@@ -23,13 +73,48 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        // justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 140,
     },
 
     text: {
-        color: 'white',
-        fontSize: 20,
+        fontSize: 30,
         fontWeight: 'bold',
+        color: 'white',
+        marginLeft: 30
+    },
+
+    input: {
+        fontSize: 18,
+        padding: 18,
+        borderWidth: 1,
+        borderColor: 'white',
+        marginTop: 30,
+        paddingLeft: 20,
+        width: '80%',
+        borderRadius: 6,
+        color: 'white'
+    },
+
+    focusInput: {
+        borderWidth: 2,
+        borderColor: '#3493d6'
+    },
+
+    nextBtn: {
+
+        backgroundColor: 'white',
+        borderRadius: 50,
+        width: '22%',
+        margin: 'auto',
+        padding: 5,
+        marginBottom: 10,
+    },
+
+    nextText: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 18,
     }
 })
